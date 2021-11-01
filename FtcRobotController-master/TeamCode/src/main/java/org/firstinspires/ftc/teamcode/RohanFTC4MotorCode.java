3e140e4160954708ae0161fd9300846a82279ca8
Nishita-23 <nishitagudipati@gmail.com>
2021-11-01 15:22:44 -0500

    test

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RohanFTC4MotorCode.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RohanFTC4MotorCode.java
@@ -1,31 +1,31 @@
-package org.firstinspires.ftc.teamcode;
+//package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.Servo;
+//import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+//import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+//import com.qualcomm.robotcore.hardware.DcMotor;
+//import com.qualcomm.robotcore.hardware.DcMotorSimple;
+//import com.qualcomm.robotcore.hardware.Servo;
 
 
 
-@TeleOp
+//@TeleOp
 
-public class RohanFTC4MotorCode extends OpMode {
+//public class RohanFTC4MotorCode extends OpMode {
 
-    DcMotor leftHorizontal ;
-    DcMotor rightHorizontal ;
-    DcMotor leftvertical ;
-    DcMotor rightvertical;
+   // DcMotor leftHorizontal ;
+   // DcMotor rightHorizontal ;
+   // DcMotor leftvertical ;
+   // DcMotor rightvertical;
 
-    public void init() {
-        leftHorizontal = hardwareMap.dcMotor.get("leftHorizontal ");
-        rightHorizontal = hardwareMap.dcMotor.get("rightHorizontal ");
-        leftvertical = hardwareMap.dcMotor.get("leftvertical ");
-        rightvertical = hardwareMap.dcMotor.get("rightvertical");
+  //  public void init() {
+    //    leftHorizontal = hardwareMap.dcMotor.get("leftHorizontal ");
+       // rightHorizontal = hardwareMap.dcMotor.get("rightHorizontal ");
+    //    leftvertical = hardwareMap.dcMotor.get("leftvertical ");
+     //   rightvertical = hardwareMap.dcMotor.get("rightvertical");
 
-    }
+    //}
 
-    public void loop(){
+   // public void loop(){
 
 
 
@@ -33,14 +33,14 @@ public class RohanFTC4MotorCode extends OpMode {
     // Forward and Backward
 
 
-        rightHorizontal .setPower(gamepad1.right_stick_x);
-        leftHorizontal.setPower(-gamepad1.left_stick_x);
+      //  rightHorizontal .setPower(gamepad1.right_stick_x);
+      //  leftHorizontal.setPower(-gamepad1.left_stick_x);
 
 
 
-        rightvertical.setPower(gamepad1.right_stick_y);
-        leftvertical.setPower(-gamepad1.right_stick_y);
+       // rightvertical.setPower(gamepad1.right_stick_y);
+       // leftvertical.setPower(-gamepad1.right_stick_y);
 
 
-    }
-}
+   // }
+//}
