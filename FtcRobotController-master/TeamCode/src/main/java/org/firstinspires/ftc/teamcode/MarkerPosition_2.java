83067f73f4014f5d17b82295fd1204725ee33e53
Nishita-23 <nishitagudipati@gmail.com>
2021-11-10 19:13:54 -0600

    Test

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MarkerPosition_2.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MarkerPosition_2.java
@@ -1,10 +1,10 @@
-/*package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor.RunMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
+//import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
 @Disabled
@@ -14,6 +14,8 @@ public class MarkerPosition extends LinearOpMode {    //Declare motors
     DcMotor Fhorizontal;
     DcMotor Bvertical;
     DcMotor Bhorizontal;
+    DcMotor UpDown;
+    Servo Claw;
 
     public void runOpMode() { //code will run once only
 
@@ -22,15 +24,68 @@ public class MarkerPosition extends LinearOpMode {    //Declare motors
         Bvertical = hardwareMap.dcMotor.get("rf");
         Fhorizontal = hardwareMap.dcMotor.get("lf");
         Bhorizontal = hardwareMap.dcMotor.get("rr");
+        UpDown = hardwareMap.dcMotor.get("");
+        Servo = hardwareMap.servo.get("");
+
 
         //set modes
         Fvertical.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         Bvertical.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         Fhorizontal.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         Bhorizontal.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        UpDown.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
 
         waitForStart();
+        if (top) {
+            DriveForwardA(0.5, 25);
+            UpDown(0.5, 25);
+            Claw(0.5, 25);
+        } else if (middle) {
+            DriveForwardA(0.5, 50);
+            UpDown(0.5, 50);
+            Claw(0.5, 50);
+        } else (bottom) {
+                DriveForwardA(0.5, 75);
+        UpDown(0.5, 50);
+        Claw(0.5, 50);
+        }
     }
-}*/
+
+    public void DriveForwardA(double power, int distance) {
+        //reset encoder
+        Fvertical.setMode(RunMode.STOP_AND_RESET_ENCODER);
+        Bvertical.setMode(RunMode.STOP_AND_RESET_ENCODER);
+
+
+        //set target position
+        Fvertical.setTargetPosition(distance * 31);
+        Bvertical.setTargetPosition(distance * 31);
+
+        Fvertical.setMode(RunMode.RUN_TO_POSITION);
+        Bvertical.setMode(RunMode.RUN_TO_POSITION);
+
+        //set power
+        Fvertical.setPower(power);
+        Bvertical.setPower(power);
+
+        while (Fvertical.isBusy() && Bvertical.isBusy()) {
+        }
+    }
+
+    public void UpDown(double power, int distance) {
+
+        //reset encoder
+        UpDown.setMode(RunMode.STOP_AND_RESET_ENCODER);
+        //set target position
+        UpDown.setTargetPosition(distance * 31);
+        UpDown.setMode(RunMode.RUN_TO_POSITION);
+        //set power;
+        UpDown.setPower(power);
+
+        while (UpDown.isBusy()) {
+        }
+    }
+}
 
 
